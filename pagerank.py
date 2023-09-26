86ef09dece725a6a43934417feb0f1467deb11b5
Poppynator <popp.daniel16@icloud.com>
2023-09-26 14:57:15 +0200

    change sys.agrv

--- pagerank.py
+++ pagerank.py
@@ -8,9 +8,9 @@ SAMPLES = 10000
 
 
 def main():
-    #if len(sys.argv) != 2:
-        #sys.exit("Usage: python pagerank.py corpus")
-    corpus = crawl("c:\\Users\\d-pop\\Documents\\Visual Studio Code\\PageRank\\PageRank\\corpus2") #(sys.argv[1]) TODO
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1]) 
     ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
     print(f"PageRank Results from Sampling (n = {SAMPLES})")
     for page in sorted(ranks):
